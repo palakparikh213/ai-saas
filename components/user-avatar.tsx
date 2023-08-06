31aaf0525754f0a880e0841b18a673323a7f366c
Palak Parikh <palakparikh213@gmail.com>
2023-08-06 02:01:09 -0400

    first commit

---
+++ components/user-avatar.tsx
@@ -0,0 +1,10 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+export const UserAvatar = () => {
+
+  return (
+    <Avatar className="h-8 w-8">
+      <AvatarImage src={"user.png"} />
+    </Avatar>
+  );
+};
